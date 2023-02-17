bd1c8ca393351716847ce13b770dc6769df7f7d6
Sufiyan Xfinite <sufiyan.memon@xfinite.io>
2023-02-17 18:47:33 +0530

    added backtracking logic for 1 cycle

---
+++ backtrack.js
@@ -0,0 +1,160 @@
+const { getTransactionOfAssetWithRole, getTransactionOfAsset } = require("./helpers/AlgoAPI");
+const { writeFile } = require("./helpers/fileOperation");
+const { check } = require('./helpers/Checker')
+
+const whiteListUserAndPool = require('./data/whitelistUser.json');
+
+
+
+/**
+ * What we need to develop
+ * - Create an function to take address or txn hash
+ * - Checks an valid address or txn hash
+ * - get all the parent addresses 
+ * - check if parents addresses are whitlist address, if found then return the addresses or else move forward
+ * - if not found then, checks the parents address of the current parent addresses
+ * - store the data  in file then for
+ */
+
+/**
+ * - main function were we will call all the sub function
+ * - function to check whitelist users and pools
+ * - looping function
+ * - function to write data in a file
+ * - counting deep we are going in 
+ */
+
+/**
+ * Funtion is the main function where all the logic meets of backtracking
+ * @param {string} address 
+ * @param {number} fromRound 
+ * @param {number} toRound
+ */
+async function main(address, fromRound, toRound){
+    let role = "receiver"
+    // get transaction 
+    let txns = await getTransactionOfAsset(address, role, fromRound, toRound);
+    writeFile(txns,'txns.json')
+    console.log("txns  ", txns.transactions.length)
+    let result = filterAndReturnSendersFromOldToNew(txns.transactions)
+    console.log("sender addresses ", result.length)
+    writeFile(result,'address.json')
+
+    let user = loopAndCheckAddress(result)
+    console.log("is white list", user)
+    // 
+    writeFile(user, 'result.json')
+
+}
+
+/**
+ * Funtion to filter all the dublicate address
+ * @param {array} transactionArray 
+ * @returns returns array of sender addresses
+ */
+function filterAndReturnSendersFromOldToNew(transactionArray){
+    let result = [], pushCount = 0
+    for(let i = transactionArray.length - 1; i >= 0; i--){
+        if(transactionArray[i]['tx-type'] == 'axfer'){ // if asset transfer txn
+
+            // console.log("asset transfer txn")
+            
+            let isOptIn = optinChecker(transactionArray[i])
+            if(!isOptIn){  // if not an optin txn
+                if(pushCount == 0){ // its an first time
+                    result.push(transactionArray[i].sender)
+                    pushCount++;
+                }else {
+                    if(result.indexOf(transactionArray[i].sender) == -1){ // checking if exist
+                        result.push(transactionArray[i].sender)
+                        pushCount++;
+                    }
+                }
+            }
+        }else if(transactionArray[i]['tx-type'] == 'appl'){ // if application call txn
+
+            // console.log("app call txn")
+            
+            let address = getAppCallAddresses(transactionArray[i])
+
+            console.log("appl ", address)
+            if(result.indexOf(address) == -1){ // checking if exist 
+                result.push(address)
+                pushCount++
+            }
+        }
+    }
+    return result
+}
+
+/**
+ * Function filter returns the pool address if any 
+ * @param {object} txnObj 
+ * @returns 
+ */
+function getAppCallAddresses(txnObj){
+    if(txnObj['inner-txns']){
+        for(let i = 0; i < txnObj['inner-txns'].length ; i++){
+            if(txnObj.sender == txnObj['inner-txns'][i]['asset-transfer-transaction'].receiver){ // if the sender is receiving token 
+                return txnObj['inner-txns'][i].sender // returning the sender 
+            }
+        }
+    } else {
+        return ""
+    }
+}
+
+/**
+ * To check address is whitelist user or not
+ * @param {string} address 
+ * @returns object which boolean and data, if it has founded whitelist user
+ */
+function checkWhiteListUserOrPool(address){
+    let data =  {hasFound: false, data: ""}
+    whiteListUserAndPool.forEach((wUser) => {
+        if(wUser.address == address){
+            data = {hasFound: true, data: wUser}
+        }
+    })
+    return data
+}
+
+/**
+ * Function loop throught the array and check the whitelist user and pools address
+ * @param {array} arrayOfAddresses 
+ * @returns empty object if not found or whitelist user infos
+ */
+function loopAndCheckAddress(arrayOfAddresses){
+    for(let i = 0 ; i < arrayOfAddresses.length ; i++){
+        let data = checkWhiteListUserOrPool(arrayOfAddresses[i])
+        if(data.hasFound){
+            return data.data
+        }
+    }
+    return {}
+}
+
+/**
+ * Function to check transaction object is optin transaction or not
+ * @param {object} txn 
+ * @returns returns bool values if the txn is optin or not
+ */
+function optinChecker(txn){
+    if(txn['tx-type'] == 'axfer'){
+        if(txn.sender == txn['asset-transfer-transaction'].receiver){
+            return true
+        }
+    }else {
+        return false
+    }
+}
+
+// let data = ["4IBBUATFUAQC53XN5YSDQLCOAS77IBX24WBFJU7S6ZHL65PAFDMACGKN6A","MG3KKJN3DPZH3XT4ENU5CXDSD6I6TW4EHF5RYYBVNFWZFLG3XC7F26LW5Y","2S7EL67TGRY2ROR3LHRVWUJE6N5KRNZ6A7KH73K2GATMNJY5SWZR53BOWA","CTKYOY6WQAKGYVOWPT4NOHJGN2I7T47HXFS4YX727B4N3FP3OIFWOIAFOM"]
+// let result =  loopAndCheckAddress(data)
+// console.log(result)
+// let data = checkWhiteListUserOrPool("MG3KKJN3DPZH3XT4ENU5CXDSD6I6TW4EHF5RYYBVNFWZFLG3XC7F26LW5Y")
+// console.log(data)
+
+// main("MG3KKJN3DPZH3XT4ENU5CXDSD6I6TW4EHF5RYYBVNFWZFLG3XC7F26LW5Y", 0 , 0)
+main("7INXNJA7X77OLCXJBL7EUAYLKDXUC474R3U4T2QPZGKKVNJPNC4OKKSSXU", 0 , 0)
+   
--- track.js
+++ track.js
@@ -23,7 +23,7 @@ let lastaddress= '', depth = 0;
  * @param {*} maxAmount 
  * @returns 
  */
-async function filterdata(address, role, minRound, maxRound,maxAmount){
+async function filterdata(address, role, minRound, maxRound, maxAmount) {
     // default obj
     let data = {
         "address": address,
@@ -35,45 +35,82 @@ async function filterdata(address, role, minRound, maxRound,maxAmount){
     }
 
     //  getting txn of specific address with define role and round
-    let txns = await getTransactionOfAsset(address,role,minRound,maxRound)
+    let txns = await getTransactionOfAsset(address, role, minRound, maxRound)
     // accessing each transaction
-    txns.transactions.forEach((txn,i) => {
+    // txns.transactions.forEach((txn,i) => {
+    for (let i = txns.transactions.length - 1; i >= 0; i--) {
+        let txn = txns.transactions[i]
+        // console.log("txn ",txn)
         // checking that we have reached the amount till we want txns
-        if(maxAmount && data['total-amount-sended'] >= maxAmount){
+        if (maxAmount && data['amount-sended'] >= maxAmount) {
+            // console.log("max amount reached !!!")
+            if (!data.toRound) {
+                data.toRound = data['receivers-address'][data['receivers-address'].length - 1]['asset-received-at-round']
+            }
             return data;
-        }else {
+        } else {
             // saving the from round
-            if(i == 0){
+            if (i == 0) {
                 data.toRound = txn['confirmed-round']
             }
             // saving the to round
-            if(i == txns.transactions.length -1 ){
+            if (i == txns.transactions.length - 1) {
                 data.fromRound = txn['confirmed-round']
             }
-            // this return if that address axist or not with index if exist
-            let checker = check(txn['asset-transfer-transaction'].receiver,data['receivers-address'])
-            // checking if exist and should not be opt-in txn
-            if((!checker.exist) && txn['asset-transfer-transaction'].receiver != data.address){
-                let subAddress = {
-                    "address": "",
-                    "amount-received": 0,
-                    "amount-sended": 0
+            // should not be opt-in txn
+            if (txn['asset-transfer-transaction'].receiver != data.address) {
+                // this return if that address axist or not with index if exist
+                let checker = check(txn['asset-transfer-transaction'].receiver, data['receivers-address'])
+
+                // console.log("values ", txn['asset-transfer-transaction'].receiver, "other ",data.address)
+                // console.log("checker ",checker, "is ",txn['asset-transfer-transaction'].receiver != data.address)
+
+                // checking if address exist 
+                if (!checker.exist) {
+                    console.log("inside if")
+                    let subAddress = {
+                        "address": "",
+                        "amount-received": 0,
+                        "amount-sended": 0,
+                        "asset-received-at-round": 0
+                    }
+                    subAddress.address = txn['asset-transfer-transaction'].receiver
+                    subAddress['amount-received'] = txn['asset-transfer-transaction'].amount
+                    subAddress['asset-received-at-round'] = txn['confirmed-round']
+                    data['receivers-address'].push(subAddress)
+                    console.log("pushed ")
+                } else { // if exist then just adding up the amount 
+                    console.log("else ")
+                    let index = checker.index
+                    // console.log("index",data['receivers-address'][index])
+                    // checking the type so we can create array of amount received
+                    // console.log("value ",data['receivers-address'][index]['amount-received']," type ",typeof(data['receivers-address'][index]['amount-received']))
+                    // if(typeof(data['receivers-address'][index]['amount-received']) == "number"){
+                    //     console.log("type is number")
+                    //     let amount = data['receivers-address'][index]['amount-received'];
+                    //     data['receivers-address'][index]['amount-received'] = [amount];
+                    //     data['receivers-address'][index]['amount-received'].push(txn['asset-transfer-transaction'].amount)
+
+                    //     // pushing the round the amount received at
+                    //     let round = data['receivers-address'][index]['asset-received-at-round']
+                    //     data['receivers-address'][index]['asset-received-at-round'] = [round]
+                    //     data['receivers-address'][index]['asset-received-at-round'].push(txn['confirmed-round'])
+
+                    // }else{// the type is already array, so just pusging the amount
+                    //     data['receivers-address'][index]['amount-received'].push(txn['asset-transfer-transaction'].amount) 
+                    //     data['receivers-address'][index]['asset-received-at-round'].push(txn['confirmed-round'])
+                    // }
+                    data['receivers-address'][index]['amount-received'] += txn['asset-transfer-transaction'].amount
                 }
-                subAddress.address = txn['asset-transfer-transaction'].receiver
-                subAddress['amount-received'] = txn['asset-transfer-transaction'].amount
-                data['receivers-address'].push(subAddress)
-            } else { // if exist then just adding up the amount 
-                let index = checker.index
-                console.log("index",data['receivers-address'][index])
-                data['receivers-address'][index]['amount-received'] += txn['asset-transfer-transaction'].amount
             }
             data['amount-sended'] += txn['asset-transfer-transaction'].amount
         }
-    })
+    }
+    // })
 
     // data['total-amount-sended']= totalAmount;
 
-    console.log("filter",data)
+    // console.log("filter",data)
     return data
 }
 
@@ -135,7 +172,7 @@ async function OrganiseData(address, role, minRound, maxRound){
 }
 
 
-
+let loopIndex = 0
 /**
  * Goal is to create an fucntion which should take address and othe para such as role and min,max round
  * What it should do :
@@ -143,32 +180,90 @@ async function OrganiseData(address, role, minRound, maxRound){
  * 2) if not then we will get the depht so we can acess the inner object by depth and save the address data to that address object
  * 3) 
  */
-async function tracker(address, role, minRound, maxRound,maxAmount){
-    console.log("result here ",resultData)
-    if(resultData.length == 0){ // which means its an first time 
+async function tracker(address, role, minRound, maxRound, maxAmount, indexAt, hasReachMaxIndex) {
+    console.log("result here ", resultData)
+    if (resultData.length == 0) { // which means its an first time 
         // getting the data and pushing it into the array
-        let data = await filterdata(address, role, minRound, maxRound,maxAmount);
+        let data = await filterdata(address, role, minRound, maxRound, maxAmount);
         resultData.push(data);
+        console.log("push", resultData)
 
         depth++; // incresing the depth so we can move to inner object
 
-        resultData[0]['receivers-address'].forEach(async (addrObj,i) => {
-            let data = await tracker(addrObj.address,'sender',resultData.fromRound,0,resultData['amount-sended'])
-            // changing the 
-            resultData['receivers-address'][i]['amount-sended'] = data['amount-sended']
-            resultData['receivers-address'][i].fromRound = data.fromRound
-            resultData['receivers-address'][i].toRound = data.toRound
-
+        // looping the addresses and calling it
+        resultData[0]['receivers-address'].forEach(async (addrObj, i) => {
+            if (i == resultData[0]['receivers-address'].length - 1) {
+                tracker(addrObj.address, 'sender', resultData[0].fromRound, 0, resultData[0]['amount-sended'], i, true)
+            } else {
+                tracker(addrObj.address, 'sender', resultData[0].fromRound, 0, resultData[0]['amount-sended'], i, false)
+            }
         })
+    } if (depth == 1) {
+        console.log('in depth 1')
+        console.log("data recived", address, minRound, indexAt, hasReachMaxIndex)
+        let data = await filterdata(address, role, minRound, maxAmount, maxAmount);
+        // console.log("data received ",data)
+        resultData[0]['receivers-address'][indexAt]['amount-sended'] = data['amount-sended']
+        resultData[0]['receivers-address'][indexAt].fromRound = data.fromRound
+        resultData[0]['receivers-address'][indexAt].toRound = data.toRound
+        resultData[0]['receivers-address'][indexAt]['receivers-address'] = data["receivers-address"]
+        console.log("final data look like", resultData[0]['receivers-address'][indexAt])
+        // has reach the last array value
+        if (hasReachMaxIndex) {
+            let arrayLength = indexAt // storing the max index to loop
+            depth++; // increase the depth 
+            // looping and calling 
+            for (let i = 0; i <= arrayLength; i++) {
+                resultData[0]['receivers-address'][i]['receivers-address'].forEach((addrObj, i) => {
+                    if (i == resultData[0]['receivers-address'][i]['receivers-address'] - 1) {
+                        tracker(addrObj.address, 'sender', resultData[0].fromRound, 0, resultData[0]['amount-sended'], i, true)
+                    } else {
+                        tracker(addrObj.address, 'sender', resultData[0].fromRound, 0, resultData[0]['amount-sended'], i, false)
+                    }
+                })
+            }
+
+        }
+    } else if (depth = 2) {
+        console.log("in depth 2")
+        console.log("data",address,role,minRound,maxRound,maxAmount)
+        let data = await filterdata(address, role, minRound, maxRound, maxAmount)
+        if (loopIndex < resultData[0]['receivers-address'].length) {
+            console.log("loop index ", loopIndex)
+            resultData[0]['receivers-address'][loopIndex]['receivers-address'][indexAt]['amount-sended'] = data["amount-sended"]
+            resultData[0]['receivers-address'][loopIndex]['receivers-address'][indexAt].fromRound = data.fromRound
+            resultData[0]['receivers-address'][loopIndex]['receivers-address'][indexAt].toRound = data.toRound
+            resultData[0]['receivers-address'][loopIndex]['receivers-address'][indexAt]['receivers-address'] = data["receivers-address"]
+        }
+        loopIndex++;
+        if (hasReachMaxIndex) {
+            depth++;
+        }
+
     }
+    // console.log("final data",resultData)
+    // console.log("inner final data",resultData[0])
+    // console.log('inner inner data', resultData[0]['receivers-address'][0]['receivers-address'])
+    writeFile(resultData,'data.json')
+    console.log("depth",depth)
 }
 
 
 
-function getData(){
-    // need to take address array and max return data 
-}
 
-// filterdata('636WHO3OAKNXPHOXDFEX5S5LSA35LWMN75JAVDLH4FOACAJSJNVHM47T7Y','sender',0,0);
+/***
+ * Logic behind the depth 
+ * we will check the depth decide how much deep we have to go in
+ * and we need to make things reusable
+ */
 
-tracker('636WHO3OAKNXPHOXDFEX5S5LSA35LWMN75JAVDLH4FOACAJSJNVHM47T7Y','sender',0,0,0)
+
+
+
+// function getData(){
+//     // need to take address array and max return data 
+// }
+
+// filterdata('TCUVIUKA56I6TU2FMTA76DB2I75LZZSDUW3QK3YIRGIJYK2JXO4SYKDNBY','sender',18062467,0);
+
+tracker('636WHO3OAKNXPHOXDFEX5S5LSA35LWMN75JAVDLH4FOACAJSJNVHM47T7Y','sender',0,0,0,0,false)
